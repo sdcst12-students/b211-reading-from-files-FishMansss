5f6fa68e050f9432084bacf34c5b540f40c3870d
Sebastiaan <159073521+FishMansss@users.noreply.github.com>
2024-11-21 11:38:34 -0800

    ee

--- task2.py
+++ task2.py
@@ -4,7 +4,7 @@ The data from this file contains 3 numbers on each line.  Determine how many lin
 Pythagorean triples are numbers where all of the sides are integers, and the 3 sides form a right triangle.
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
-
+import math
 filename = "task02a.txt"
 file = open(filename,'r')
 filename1 = "task02b.txt"
@@ -15,5 +15,20 @@ fileContents1 = file1.read()
 
 cContents = fileContents + fileContents1
 sContents = cContents.split('\n')
-Contents = sContents.pop("' '")
+Contents = sContents.remove("")
+
 print(Contents)
+
+"""
+pytry = [] 
+
+
+for i in sContents:
+    if (i**2 + (i+1)**2) == (i+3)**2:
+        pytry.append(i)
+        pytry.append(i+1)
+        pytry.append(i+2)
+        
+print(pytry)
+        
+"""
