5801ee31b6ef3ee1fa3f77dbe3d1d2d1e8d1c4af
Sebastiaan <159073521+FishMansss@users.noreply.github.com>
2024-11-21 11:23:41 -0800

    e

--- task1.py
+++ task1.py
@@ -9,9 +9,21 @@ Example:
 assert find('apple') == 0
 assert find('fish') == 5
 '''
-def find(needle):
-    pass
 
+
+
+
+def find(needle):
+    filename = 'task01.txt'
+    file = open(filename,"r")
+    fileContents = file.read()   
+    x = fileContents.split('\n')
+    z = 0
+    for i in x:
+        z += 1
+        if i == needle:
+         print(z-1)
+        
 
 if __name__ == "__main__":
     assert find('apple') == 0
--- task2.py
+++ task2.py
@@ -4,3 +4,16 @@ The data from this file contains 3 numbers on each line.  Determine how many lin
 Pythagorean triples are numbers where all of the sides are integers, and the 3 sides form a right triangle.
 The triples contained are : { 2a : 6, 2b: 4, 2c: 11}
 """
+
+filename = "task02a.txt"
+file = open(filename,'r')
+filename1 = "task02b.txt"
+file1 = open(filename1,'r')
+
+fileContents = file.read()
+fileContents1 = file1.read()
+
+cContents = fileContents + fileContents1
+sContents = cContents.split('\n')
+Contents = sContents.pop("' '")
+print(Contents)
