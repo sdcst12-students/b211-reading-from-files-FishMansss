efbecf07a8acea6d4d5ab07636d6517a53e65111
Sebastiaan <159073521+FishMansss@users.noreply.github.com>
2024-11-29 10:20:11 -0800

    e

--- task3.py
+++ task3.py
@@ -7,4 +7,22 @@ Read the contents of task03.txt into your program and determine the points value
 
 For sample data task03.txt, the largest sum should be 68787
 """
+import math
+## importing file ans splitting one format
+subFile= []
+fileImport = 'task03.txt'
+fileRead = open(fileImport, 'r')
+file = fileRead.read()
+sFile = file.split('\n')
+
+
+## indexing and splitting file into subfiles on empty elements
+lastIndex = -1
+for i in range(len(sFile)):
+    if sFile[i] == '':
+        subFile.append(sFile[lastIndex +1 : i ])
+        lastIndex = i
+
+## find greatest sum
+greatestSum=0
 
